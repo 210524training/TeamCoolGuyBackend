aad5e227e1a9b64a69ffe6e5497ccb49f0eff1de
charleshardy1 <charlesgoffhardy@gmail.com>
2021-07-21 14:52:32 -0700

    change interval

--- tcswap-backend/serverless.ts
+++ tcswap-backend/serverless.ts
@@ -58,6 +58,7 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
     lambdaHashingVersion: '20201221',
+    timeout: 50,
   },
   
   // import the function via paths
